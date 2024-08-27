cda0957d49fa6345cfa2ec2ca65b3617d48844ec
Srivarsne <150252962+Srivarsne@users.noreply.github.com>
2024-08-27 15:22:36 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,18 @@
+a=7
+b=5
+def add(a,b):
+    c=a+b
+    return c
+def sub(a,b):
+    c=a-b
+    return c
+def mul(a,b):
+    c=a*b
+    return c
+def div(a,b):
+    c=a/b
+    return c
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
